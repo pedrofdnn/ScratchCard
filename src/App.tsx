364eaf1eacaba5b8d74daf0a15e0d1e30abf09ba
Pedro Fernandes <pedrofdnn@gmail.com>
2024-10-16 16:56:38 -0300

    Realizando Deploy em tempo real

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import HomePage from "./Components/Home/Index";
 export default function App() {
   return (
     <div>
-      <HomePage id={0} image={""} onScratch={function (id: number): void {
+      <HomePage id={0} image={""} onScratch={function (): void {
         throw new Error("Function not implemented.");
       }} />
     </div>
