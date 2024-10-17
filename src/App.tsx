b453497e1b747a821fd988b4005109ffa6292f33
Pedro Fernandes <pedrofdnn@gmail.com>
2024-10-16 23:54:42 -0300

    Configuração do Layout inicial concluida.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import HomePage from "./Components/Home/Index";
+import './index.scss'
 
 export default function App() {
   return (
-    <div>
+    <div className="pageDefault">
       <HomePage id={0} image={""} onScratch={function (): void {
         throw new Error("Function not implemented.");
       }} />
