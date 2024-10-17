75d20b13b4e0d9d76e43bd85dd49fa8ea877c795
Pedro Fernandes <pedrofdnn@gmail.com>
2024-10-17 19:59:07 -0300

    implantação da função de raspagem instalada.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import HomePage from "./Components/Home/Index";
 import './index.scss'
 
 export default function App() {
+  const handleScratch = (id: number) => {
+    console.log(`Card ${id} foi riscado`);
+  };
   return (
     <div>
-      <HomePage id={0} image={""} onScratch={function (): void {
-        throw new Error("Function not implemented.");
-      }} />
+      <HomePage id={0}
+        image={""}
+        onScratch={handleScratch} />
     </div>
   )
 }
--- src/Components/Cards/index.tsx
+++ src/Components/Cards/index.tsx
@@ -1,23 +1,32 @@
-import '../../App.scss'
+import ScratchCard from 'react-scratchcard-v2-temiyores-fork';
+import coverImage from '../../Assets/img/COVER.png';
+import hiddenImage from '../../Assets/img/bag.png';
 
-interface CardsProps {
+
+interface ScratchCardProps {
   id: number;
   image: string;
   onScratch: (id: number) => void;
 }
 
-export default function ScratchCard({ id, image, onScratch }: CardsProps) {
+const scratchCardSettings = {
+  width: 300,
+  height: 150,
+  image: coverImage,
+  finishPercent: 80, 
+  onComplete: () => ('') 
+};
 
+export default function Scratch({ id, image, onScratch }: ScratchCardProps) {
   const handleScratch = () => {
     onScratch(id);
   };
 
   return (
-    <div className='cardbox'>      
-      <div className="scratch-card" onClick={handleScratch}>
-        <img src={image} alt={`Card ${id}`} className={'cardsImage'} />
-      </div>
-
+    <div className="scratch-card-container">
+      <ScratchCard color={''} {...scratchCardSettings}>
+        <img src={image || hiddenImage} alt={`Card ${id}`} className="cardsImage" />
+      </ScratchCard>
     </div>
-  )
+  );
 }
